f2e9f9c761ff49b49c5317e03300d381c4f75a84
Alex Coomans <alexc@squareup.com>
2017-08-04 12:37:54 -0700

    Normalize abnormal whitespace in PEM certificates

--- lib/rails/auth/x509/filter/pem.rb
+++ lib/rails/auth/x509/filter/pem.rb
@@ -5,7 +5,14 @@ module Rails
         # Extract OpenSSL::X509::Certificates from Privacy Enhanced Mail (PEM) certificates
         class Pem
           def call(pem)
-            OpenSSL::X509::Certificate.new(pem.delete("\t")).freeze
+            # Normalize the whitespace in the certificate to the exact format
+            # certificates are normally formatted in otherwise parsing with fail
+            # with a 'nested asn1 error'. split(" ") handles sequential whitespace
+            # characters like \t, \n, and space.
+            OpenSSL::X509::Certificate.new(pem.split(" ").instance_eval do
+              [[self[0], self[1]].join(" "), self[2...-2], [self[-2], self[-1]].join(" ")]
+                .flatten.join("\n")
+            end).freeze
           end
         end
       end
