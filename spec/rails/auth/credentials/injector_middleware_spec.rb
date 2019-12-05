bc10c1ae98aab3cf1a5579d4dedbf7b9f5868fa7
Alex Coomans <alexc@squareup.com>
2019-12-05 12:40:41 -0600

    Allow dynamic injection of credentials

--- spec/rails/auth/credentials/injector_middleware_spec.rb
+++ spec/rails/auth/credentials/injector_middleware_spec.rb
@@ -10,4 +10,17 @@ RSpec.describe Rails::Auth::Credentials::InjectorMiddleware do
     _response, env = middleware.call(request)
     expect(env[Rails::Auth::Env::CREDENTIALS_ENV_KEY]).to eq credentials
   end
+
+  context "with a proc for credentials" do
+    let(:credentials_proc) { instance_double(Proc) }
+    let(:middleware)       { described_class.new(app, credentials_proc) }
+
+    it "overrides rails-auth credentials in the rack environment" do
+      expect(credentials_proc).to receive(:call).with(request).and_return(credentials)
+
+      _response, env = middleware.call(request)
+
+      expect(env[Rails::Auth::Env::CREDENTIALS_ENV_KEY]).to eq credentials
+    end
+  end
 end
